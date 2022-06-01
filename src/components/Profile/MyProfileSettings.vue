ff5749c27614aba55457ce98f476e0824410df3a
Mac <mac@Mac-mini.local>
2022-06-01 23:41:06 +0300

    mobile edit page

--- src/components/Profile/MyProfileSettings.vue
+++ src/components/Profile/MyProfileSettings.vue
@@ -26,8 +26,8 @@
                 placeholder="Bomba@mail.com"
                 disabled
               />
-              <input class="code__input" type="text" maxlength="4" />
-              <div class="resend__block">
+              <!-- <input class="code__input" type="text" maxlength="4" />
+              <div class="resend__block ">
                 <img
                   src="../../assets/images/main/myprofile__settings__resend.svg"
                   alt=""
@@ -40,20 +40,21 @@
                   src="../../assets/images/main/myprofile__settings__done.svg"
                   alt=""
                 />
-              </div>
+              </div> -->
             </div>
+            <div class="horizontal__line"></div>
           </div>
           <span class="span__desc hide"
             >Необходимо подтвердить смену учетной записи. Введите код полученный
             из сообщения, которого мы вам отправили.</span
           >
+
           <div class="settings change__phone">
             <div class="settings__desc">
               <img
                 src="@/assets/images/main/myprofile__settings__phone.svg"
                 alt=""
               />
-
               <div class="title">Номер телефона:</div>
             </div>
             <div class="inputs__box">
@@ -63,8 +64,10 @@
                 placeholder="+7 (435) 495 59 34"
                 disabled
               />
+
               <div class="send__phone__code hide">Отправить</div>
-              <div class="sms__code">
+
+              <div class="sms__code hide">
                 <input class="code__input" type="text" maxlength="4" />
                 <div class="resend__block">
                   <img
@@ -82,8 +85,9 @@
                 </div>
               </div>
             </div>
+            <div class="horizontal__line"></div>
           </div>
-          <div class="settings hide">
+          <div class="settings">
             <div class="settings__desc">
               <img
                 src="@/assets/images/main/myprofile__settings__lock.svg"
@@ -99,7 +103,7 @@
               disabled
             />
           </div>
-          <div class="settings change__password">
+          <div class="settings change__password hide">
             <div class="settings__desc">
               <img
                 src="@/assets/images/main/myprofile__settings__lock.svg"
@@ -138,10 +142,10 @@
         </div>
         <div class="horizontal__line"></div>
         <div class="settings__footer">
-          <div class="logout hide">Выйти из аккаунта</div>
-          <div class="delete__profile hide">Удалить профиль</div>
-          <div class="cancel__changes">Отмена</div>
-          <div class="save__changes">Сохранить</div>
+          <div class="logout">Выйти из аккаунта</div>
+          <div class="delete__profile">Удалить профиль</div>
+          <div class="cancel__changes hide">Отмена</div>
+          <div class="save__changes hide">Сохранить</div>
         </div>
       </div>
     </div>
@@ -158,17 +162,17 @@ export default {};
 }
 .page__background {
   height: 100%;
-  width: 100%;
+  width: 100vw;
   display: flex;
   justify-content: center;
   .settings__container {
     @extend .flex__center;
     justify-content: space-between;
     flex-direction: column;
-    width: 55%;
+    width: 62.53vw;
     height: 100vh;
     background: rgba(196, 196, 196, 0.05);
-    border: 1px solid rgba(255, 255, 255, 0.33);
+    border: 0.06vw solid rgba(255, 255, 255, 0.33);
     border-top: none;
     border-bottom: none;
     .settings__content {
@@ -176,48 +180,48 @@ export default {};
       justify-content: space-between;
       flex-direction: column;
       align-items: center;
-      height: 525px;
-      margin-top: 70px;
+      height: 31.62vw;
+      margin-top: 4.21vw;
       .horizontal__line {
-        width: 785px;
+        width: 47.289vw;
         height: 0px;
-        border: 1px solid rgba(255, 255, 255, 0.14);
+        border: 0.06vw solid rgba(255, 255, 255, 0.14);
       }
       .settings__header {
         @extend .flex__center;
         flex-direction: column;
         h1 {
           font-weight: 600;
-          font-size: 18px;
+          font-size: 1.084vw;
           line-height: 153.5%;
           display: flex;
           align-items: center;
           color: #ffffff;
-          margin-bottom: 12px;
+          margin-bottom: 0.722vw;
         }
         .horizontal__line {
-          width: 847px;
+          width: 51vw;
           height: 0px;
-          border: 1px solid rgba(255, 255, 255, 0.33);
+          border: 0.06vw solid rgba(255, 255, 255, 0.33);
         }
       }
       .settings__body {
-        width: 598px;
-        height: 232px;
+        width: 36vw;
+        height: 14vw;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-        margin-right: 180px;
+        margin-right: 10.84vw;
         .span__desc {
           font-weight: 600;
-          font-size: 12px;
+          font-size: 0.722vw;
           line-height: 132.5%;
           display: flex;
           align-items: center;
           color: rgba(255, 255, 255, 0.4);
-          width: 370px;
-          height: 32px;
-          margin-left: 272px;
+          width: 22.289vw;
+          height: 1.92vw;
+          margin-left: 16.38vw;
           &.hide {
             display: none;
           }
@@ -231,24 +235,27 @@ export default {};
   justify-content: space-between;
   display: flex;
   &.change__mail {
-    width: 825px;
+    width: 49.698vw;
+    .horizontal__line {
+      display: none;
+    }
     .inputs__box {
-      width: 552px;
+      width: 33.25vw;
       .block {
         display: flex;
         align-items: center;
         justify-content: space-between;
       }
       .code__input {
-        padding: 18px 50px;
-        width: 136px;
-        height: 59px;
-        border: 1px solid #ffffff;
-        border-radius: 11px;
+        padding: 1.084vw 3.012vw;
+        width: 8.192vw;
+        height: 3.554vw;
+        border: 0.06vw solid #ffffff;
+        border-radius: 0.662vw;
         background: none;
         outline: none;
         font-weight: 600;
-        font-size: 15px;
+        font-size: 0.903vw;
         line-height: 153.5%;
         display: flex;
         align-items: center;
@@ -277,60 +284,66 @@ export default {};
     }
   }
   &.change__phone {
-      width: 825px;
+    .horizontal__line {
+      display: none;
+    }
+    width: 49.698vw;
     .inputs__box {
-      width: 552px;
+      width: 33.25vw;
       .sms__code {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          width: 215px;
-           .code__input {
-        padding: 18px 50px;
-        width: 136px;
-        height: 59px;
-        border: 1px solid #ffffff;
-        border-radius: 11px;
-        background: none;
-        outline: none;
-        font-weight: 600;
-        font-size: 15px;
-        line-height: 153.5%;
         display: flex;
         align-items: center;
-        color: #ffffff;
+        justify-content: space-between;
+        width: 12.95vw;
         &.hide {
           display: none;
         }
-      }
-      .vertical__line {
-        margin-top: 0;
-        &.hide {
-          display: none;
+        .code__input {
+          padding: 1.084vw 3.012vw;
+          width: 8.19vw;
+          height: 3.554vw;
+          border: 0.06vw solid #ffffff;
+          border-radius: 0.662vw;
+          background: none;
+          outline: none;
+          font-weight: 600;
+          font-size: 0.903vw;
+          line-height: 153.5%;
+          display: flex;
+          align-items: center;
+          color: #ffffff;
+          &.hide {
+            display: none;
+          }
         }
-      }
-      .logo__done {
-        margin-top: 0;
-        &.hide {
-          display: none;
+        .vertical__line {
+          margin-top: 0;
+          &.hide {
+            display: none;
+          }
         }
-      }
-      .resend__block {
-        &.hide {
-          display: none;
+        .logo__done {
+          margin-top: 0;
+          &.hide {
+            display: none;
+          }
         }
-      }
+        .resend__block {
+          &.hide {
+            display: none;
+          }
+        }
       }
     }
   }
   .send__phone__code {
     @extend .flex__center;
-    border: 1px solid #ffffff;
-    border-radius: 11px;
-    width: 174px;
-    height: 60px;
+    border: 0.06vw solid #ffffff;
+    border-radius: 0.662vw;
+    width: 10.48vw;
+    height: 3.61vw;
     font-weight: 600;
-    font-size: 15px;
+    font-size: 0.903vw;
     line-height: 153.5%;
     color: #ffffff;
     &.hide {
@@ -341,7 +354,7 @@ export default {};
     display: none;
   }
   &.change__password {
-    width: 815px;
+    width: 49.09vw;
     &.hide {
       display: none;
     }
@@ -351,49 +364,49 @@ export default {};
     align-items: center;
   }
   .inputs__box {
-    width: 544px;
-    height: 84px;
+    width: 32.77vw;
+    height: 5.06vw;
     display: flex;
     justify-content: space-between;
     align-items: center;
     .vertical__line {
-      margin-top: 30px;
+      margin-top: 1.807vw;
       width: 0px;
-      height: 40px;
-      border: 1px solid rgba(255, 255, 255, 0.2);
+      height: 2.409vw;
+      border: 0.06vw solid rgba(255, 255, 255, 0.2);
     }
     .logo__done {
-      margin-top: 30px;
+      margin-top: 1.807vw;
     }
   }
   .title {
     font-weight: 500;
-    font-size: 14px;
+    font-size: 0.8433vw;
     line-height: 153.5%;
     color: rgba(255, 255, 255, 0.4);
-    margin-left: 12px;
+    margin-left: 0.722vw;
   }
   .input__block {
-    width: 250px;
-    height: 84px;
+    width: 15.06vw;
+    height: 5.06vw;
     span {
       font-weight: 600;
-      font-size: 12px;
+      font-size: 0.722vw;
       line-height: 132.5%;
       color: rgba(255, 255, 255, 0.4);
     }
   }
   .input {
-    width: 326px;
-    height: 60px;
-    border: 1px solid rgba(255, 255, 255, 0.14);
-    border-radius: 11px;
+    width: 19.638vw;
+    height: 3.61vw;
+    border: 0.06vw solid rgba(255, 255, 255, 0.14);
+    border-radius: 0.662vw;
     background: none;
     outline: none;
-    padding: 12px;
+    padding: 0.722vw;
     &.change__password {
-      margin-top: 8px;
-      width: 250px;
+      margin-top: 0.48vw;
+      width: 15.06vw;
     }
     &::placeholder {
       color: #ffffff;
@@ -403,30 +416,33 @@ export default {};
 .settings__footer {
   display: flex;
   justify-content: space-between;
-  width: 788px;
+  width: 47.4698vw;
   .logout {
-    padding: 7px 13px;
-    height: 33px;
-    border: 1px solid rgba(255, 255, 255, 0.14);
-    border-radius: 11px;
+    padding: 0.42vw 0.783vw;
+    height: 2vw;
+    border: 0.06vw solid rgba(255, 255, 255, 0.14);
+    border-radius: 0.662vw;
     font-weight: 700;
-    font-size: 14px;
+    font-size: 0.8433vw;
     line-height: 132.5%;
     color: #ffffff;
+    cursor: pointer;
+
     &.hide {
       display: none;
     }
   }
   .delete__profile {
     display: inline-flex;
-    padding: 7px 13px;
-    height: 33px;
-    border: 1px solid #3e74ff;
-    border-radius: 11px;
+    padding: 0.421vw 0.783vw;
+    height: 1.9879vw;
+    border: 0.06vw solid #3e74ff;
+    border-radius: 0.662vw;
     font-weight: 700;
-    font-size: 14px;
+    font-size: 0.8433vw;
     line-height: 132.5%;
     color: #3e74ff;
+    cursor: pointer;
     &.hide {
       display: none;
     }
@@ -434,10 +450,10 @@ export default {};
   .cancel__changes {
     display: flex;
     align-items: center;
-    padding: 7px 13px;
-    height: 33px;
-    border: 1px solid rgba(255, 255, 255, 0.14);
-    border-radius: 11px;
+    padding: 0.421vw 0.7831vw;
+    height: 2vw;
+    border: 0.06vw solid rgba(255, 255, 255, 0.14);
+    border-radius: 0.662vw;
     &.hide {
       display: none;
     }
@@ -445,12 +461,91 @@ export default {};
   .save__changes {
     display: flex;
     align-items: center;
-    padding: 7px 13px;
-    height: 33px;
-    border: 1px solid rgba(255, 255, 255, 0.14);
-    border-radius: 11px;
+    padding: 0.421vw 0.783vw;
+    height: 2vw;
+    border: 0.06vw solid rgba(255, 255, 255, 0.14);
+    border-radius: 0.66vw;
     &.hide {
       display: none;
+    }
+  }
+}
+@media (max-width: 1200px) {
+  .auth__back__btn {
+    z-index: 1;
+    top: 48px;
+  }
+  .page__background {
+    .settings__container {
+      // width: 375px;
+      width: 100vw;
+
+      border: none;
+      background: none;
+      .settings__content {
+        width: 89.33vw;
+        // width: 335px;
+        margin: 0;
+        // height: 546px;
+        height: 145.6vw;
+        .horizontal__line {
+          display: flex;
+          // width: 335px;
+        width: 89.33vw;
+
+          border: 1px solid rgba(255, 255, 255, 0.05);
+        }
+        .settings__header {
+          // width: 375px;
+          width: 100vw;
+          // height: 89px;
+          height: 23.73vw;
+          background: rgba(255, 255, 255, 0.05);
+          backdrop-filter: blur(30px);
+          justify-content: end;
+          .horizontal__line {
+            // width: 375px;
+            width: 100vw;
+          }
+        }
+        .settings__body {
+          // width: 335px;
+        width: 89.33vw;
+          // height: 355px;
+          height: 94.66vw;
+          margin: 0;
+          .settings {
+            flex-direction: column;
+            // width: 335px;
+        width: 89.33vw;
+
+            .settings__desc {
+              img {
+                display: none;
+              }
+              .title {
+                margin: 0;
+                color: #ffffff;
+              }
+            }
+            .inputs__box {
+              // width: 335px;
+        width: 89.33vw;
+
+            }
+            &.change__mail {
+              // width: 335px;
+        width: 89.33vw;
+
+            }
+          }
+        }
+        .settings__footer {
+          // width: 335px;
+        width: 89.33vw;
+
+        }
+      }
     }
   }
 }
